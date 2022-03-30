58308a0ee4bf8b5b99bb97b0b7e7d37278076cbd
PC-03 <PC-03@DESKTOP-U8MF28Q>
2022-03-30 08:32:25 -0400

    Clase Estudiantes

---
+++ mavenproject1/src/main/java/ups/edu/ec/mavenproject1/Estudiante.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ups.edu.ec.mavenproject1;
+
+public class Estudiante {
+    
+    String nombre;
+    int calificacion;
+    String carrera;
+    double peso;
+    
+}
