59bcbfb89f199f45b96424815ceb30119e2f0f8b
PC-03 <PC-03@DESKTOP-U8MF28Q>
2022-04-01 08:43:34 -0400

    ejercicio1

--- Objetos/src/main/java/celular.java
+++ Objetos/src/main/java/celular.java
@@ -8,5 +8,34 @@
  * @author PC-03
  */
 public class celular {
+    String marca;
+    String modelo;
+    int año;
+    float precio;
     
+        public static void main(String[] args) {
+        celular celular1 = new celular();
+        
+        celular1.marca= "SAMSUNG";
+        celular1.modelo= "CRISTAL GULL HD";
+        celular1.año= 2020;
+        celular1.precio= 500;
+        
+            System.out.println("El nombre del telefono es: "+celular1.marca);
+            System.out.println("El modelo es: "+celular1.modelo);
+            System.out.println("El el año es: "+celular1.año);
+            System.out.println("su valor es: "+celular1.precio);
+            
+            
+            celular1.marca= "REDMI";
+            celular1.modelo= "S9";
+            celular1.año= 2021;
+            celular1.precio= 250;
+        
+            System.out.println("El nombre del telefono es: "+celular1.marca);
+            System.out.println("El modelo es: "+celular1.modelo);
+            System.out.println("El el año es: "+celular1.año);
+            System.out.println("su valor es: "+celular1.precio);
+    }
 }
+
